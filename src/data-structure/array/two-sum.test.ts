dcc9ba7ae40fe641d0753243c0b0d19ff0ab192b
xq <xq@xqdeMacBook-Pro.local>
2022-05-07 17:17:46 +0800

    feat: 添加 vitest 准备开始 coding

---
+++ src/data-structure/array/two-sum.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest'
+import { twoSum } from './two-sum'
+
+test('two-sum', () => {
+  expect(twoSum()).toBe(2)
+})
---
+++ src/data-structure/array/two-sum.ts
@@ -0,0 +1,3 @@
+export function twoSum() {
+  return 2
+}
